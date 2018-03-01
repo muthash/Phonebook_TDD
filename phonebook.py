90d4200d227ef494faf25a13977b54338dd6c698
Muthash <kathingo92@gmail.com>
2018-03-01 11:34:34 +0300

    Add delete method functionaly

--- phonebook.py
+++ phonebook.py
@@ -10,4 +10,15 @@ class Phonebook():
         self.phonebook[self.name] = self.number
         if self.phonebook[self.name] == number:
             return "contact added successfully"
-        return 0    
+        return 0  
+
+    def delete(self, name):
+        self.name = name
+        contact_available = name in self.phonebook.keys()
+        if contact_available:
+            del self.phonebook[self.name]
+            contact = name in self.phonebook.keys()
+            if not contact:
+                return "contact deleted successfully"
+            return "contact not deleted"
+        return 0
