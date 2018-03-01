f43ac5c040efd3cdbdd37a0c128b1679a3dbafe4
Muthash <kathingo92@gmail.com>
2018-03-01 10:58:31 +0300

    add test contact addition test case

--- test_phonebook.py
+++ test_phonebook.py
@@ -8,4 +8,10 @@ class PhonebookTestCase(unittest.TestCase):
     """Test case for the phonebook app"""
     def setUp(self):
         self.name = "steve"
-        self.number = 0712762717
+        self.number = "0712762717"
+
+    def test_add(self):
+        """Test user can add a new contact"""
+        result = book.add(self.name, self.number)
+        self.assertEqual(result, "contact added successfully")
+
